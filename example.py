9dd18ab7da099ccba0d853e77454c9984d9588f3
kyang <1627528922@qq.com>
2019-09-07 16:59:16 +0800

    first commit

---
+++ example.py
@@ -0,0 +1,9 @@
+from main_in_one import *
+
+class exmp():
+
+    def __init__(self):
+        self.train_path = './data/enh_p4653_3sets.h5'
+        self.model_path = './models/model_p4653.h5'
+        self.test_path = './data/vista_human_3sets.h5'
+
